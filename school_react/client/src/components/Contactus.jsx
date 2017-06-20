b39ca44a285486fc652c906eca5164edab59a2db
Rajini <rajinipoosa@gmail.com>
2017-06-20 21:59:34 +0100

    changed the contacts file

--- school_react/client/src/components/Contactus.jsx
+++ school_react/client/src/components/Contactus.jsx
@@ -1,16 +1,6 @@
 import React from 'react'
-import GoogleMapReact from 'google-map-react';
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 class Contactus extends React.Component{
-   constructor(props){
-    super(props)
-    this.state = {
-      center: {lat: 52.93, lng: -3.29},
-      zoom: 11
-    
-  }
-}
   
 
   render(){
@@ -20,18 +10,7 @@ class Contactus extends React.Component{
        <p>Wester Broom Place</p>
        <p>EH12 7RT</p>
        <p>4343454646</p>
-     
-     <GoogleMapReact className = "gmap"
-     
-       defaultCenter={this.state.center}
-       defaultZoom={this.state.zoom}
-     >
-       <AnyReactComponent
-         lat={55.937914}
-         lng={-3.295366}
-         text={'Edinburgh'}
-       />
-     </GoogleMapReact>
+
       </div>)
   }
   
